74dd31d337a0ee03e8c07d9c99b298f534b20202
Marsel <m.gaynatullin@mail.ru>
2025-08-28 23:17:59 +0300

    added mikro orm config

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -5,9 +5,9 @@ import { ConfigService } from '@nestjs/config';
 
 export function createMikroOrmConfig(config: ConfigService): Options<PostgreSqlDriver> {
   return {
-    dbName: config.get<string>('DB_NAME') || 'multimodal_bot',
-    user: config.get<string>('DB_USER') || 'user',
-    password: config.get<string>('DB_PASSWORD') || 'user',
+    dbName: config.get<string>('DB_NAME'),
+    user: config.get<string>('DB_USER'),
+    password: config.get<string>('DB_PASSWORD'),
     driver: PostgreSqlDriver,
     debug: true,
     entities: ['dist/**/*.entity.js'],
