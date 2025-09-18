fe58b864beb9085aa74ab6a337668bb9febb13aa
Marsel <m.gaynatullin@mail.ru>
2025-09-18 16:57:11 +0300

    Add video and video note handling

--- src/telegram/services/bot/bot-handler-registration.service.ts
+++ src/telegram/services/bot/bot-handler-registration.service.ts
@@ -9,10 +9,12 @@ import { PhotoHandlerService } from 'src/telegram/services/photo-handler.service
 import { VoiceHandlerService } from 'src/telegram/services/voice-handler.service';
 import { AudioHandlerService } from 'src/telegram/services/audio-handler.service';
 import { RegisterCommandsDeps } from 'src/telegram/commands/utils';
+import { I18nService } from 'src/i18n/i18n.service';
 
 @Injectable()
 export class BotHandlerRegistrationService {
     constructor(
+        private readonly i18n: I18nService,
         private readonly messageHandler: MessageHandlerService,
         private readonly documentHandler: DocumentHandlerService,
         private readonly photoHandler: PhotoHandlerService,
@@ -28,6 +30,18 @@ export class BotHandlerRegistrationService {
         bot.on('message:photo', (ctx) => this.photoHandler.handlePhoto(ctx));
         bot.on('message:voice', (ctx) => this.voiceHandler.handleVoice(ctx));
         bot.on('message:audio', (ctx) => this.audioHandler.handleAudio(ctx));
+        bot.on('message:video', async (ctx) => {
+            try {
+                const userLang = ctx.session?.lang || this.i18n.getDefaultLocale();
+                await ctx.reply(this.i18n.t('warning_unsupported_file_type', userLang));
+            } catch { }
+        });
+        bot.on('message:video_note', async (ctx) => {
+            try {
+                const userLang = ctx.session?.lang || this.i18n.getDefaultLocale();
+                await ctx.reply(this.i18n.t('warning_unsupported_file_type', userLang));
+            } catch { }
+        });
         bot.catch((err) => this.messageHandler.handleError(err));
     }
 }
